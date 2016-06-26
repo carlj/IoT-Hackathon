eb26a697e2ad222f1dfafc8a3d49560b735f152e
Carl Jahn <jahncarl@gmail.com>
2016-06-26 17:55:19 +0200

    addex executable flag to install scripts

--- helper/install.sh
+++ helper/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+## Install Iot-Hackathon PIP package
 sudo pip install pika
 
 cd ./library/iot-hackathon
 sudo python setup.py install
+
+## Install services
+cd ./services
+sudo ./install.sh
