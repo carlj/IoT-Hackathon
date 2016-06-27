73be350d9ff0977307bae88fc70543ec5cfbaf7f
Carl Jahn <jahncarl@gmail.com>
2016-06-27 12:19:35 +0200

    moved files

--- helper/install-library.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-## Install Iot-Hackathon PIP package
-sudo pip install pika
-
-cd ./library/IoT_Hackathon
-sudo python setup.py install
---
+++ sensors/install-sensors.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+# Install general packages
+sudo pip install RPi.GPIO
+#sudo pip install picamera
+#sudo pip install requests
+#sudo pip install pygame
+#sudo pip install pygameui
+
+# Install the LSM303 sources
+sudo ./LSM303/install-lsm303.sh
+
+# Install the MTK3339 sources
+sudo ./MTK3339/install-mtk3339.sh
+
+# Install the DHT22 sources
+sudo ./DHT22/install-dht22.sh
+
+# Install the L3GD20 sources
+sudo ./L3GD20/install-l3gd20.sh
+
+# Install the TSL2591 sources
+sudo ./TSL2591/install-tsl2591.sh
