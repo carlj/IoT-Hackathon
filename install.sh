dcbe0c74e92b448d1cff3ce3a6b8def84697008d
Carl Jahn <jahncarl@gmail.com>
2016-06-27 14:25:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@ cd "$(dirname "$0")"
 
 # Install I2C needed packages
 sudo apt-get update
-sudo apt-get install python-smbus
-sudo apt-get install i2c-tools
+sudo apt-get install --yes python-smbus
+sudo apt-get install --yes i2c-tools
 
 # Install Python GPIO support
-sudo apt-get install python-dev
-sudo apt-get install python-rpi.gpio
+sudo apt-get install --yes python-dev
+sudo apt-get install --yes python-rpi.gpio
 
 # Install RabbitMQ
 echo 'deb http://www.rabbitmq.com/debian/ testing main' | sudo tee /etc/apt/sources.list.d/rabbitmq.list
 wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | sudo apt-key add -
-sudo apt-get install rabbitmq-server
+sudo apt-get install --yes rabbitmq-server
 
 sudo cp ./helper/rabbitmq/rabbitmq.config /etc/rabbitmq/rabbitmq.config
 
